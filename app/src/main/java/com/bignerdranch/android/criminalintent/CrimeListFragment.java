623a35e2dc480c2084ddad777f9cdd4ffe008728
Martijn Kortenhoeven <m.kortenhoeven@gmail.com>
2017-12-10 22:34:01 +0100

    Werkend t/m pagina 166. Laat een blank scherm zien met CrimeListActivity.

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by mkort on 10-12-2017.
+ */
+
+public class CrimeListFragment extends Fragment {
+
+    // Nothing yet.
+}
